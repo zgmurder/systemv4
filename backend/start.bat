c0961264b646a5f02acef1d0a853e2201f317eac
ShunliZheng <zhengsl@nicetech-video.com>
2019-06-19 14:49:05 +0800

    启动脚本

---
+++ backend/start.bat
@@ -0,0 +1,6 @@
+@echo off
+start cmd /k java -jar ./api-gateway/target/api-gateway-1.0-SNAPSHOT-fat.jar -conf ./api-gateway/src/config/local.json
+start cmd /k java -jar ./account-microservice/target/account-microservice-1.0-SNAPSHOT-fat.jar  -conf ./account-microservice/src/config/local.json
+start cmd /k java -jar ./dictionary-microservice/target/dictionary-microservice-1.0-SNAPSHOT-fat.jar  -conf ./dictionary-microservice/src/config/local.json
+start cmd /k java -jar ./resource-microservice/target/resource-microservice-1.0-SNAPSHOT-fat.jar  -conf ./resource-microservice/src/config/local.json
+start cmd /k java -jar ./standard-microservice/target/standard-microservice-1.0-SNAPSHOT-fat.jar  -conf ./standard-microservice/src/config/local.json
