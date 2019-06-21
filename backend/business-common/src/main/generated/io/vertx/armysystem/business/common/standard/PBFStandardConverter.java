6d585779b3b21ab93b43569f2e2bff68716be345
ShunliZheng <zhengsl@nicetech-video.com>
2019-06-21 11:14:04 +0800

    添加体型标准表

---
+++ backend/business-common/src/main/generated/io/vertx/armysystem/business/common/standard/PBFStandardConverter.java
@@ -0,0 +1,79 @@
+package io.vertx.armysystem.business.common.standard;
+
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.json.JsonArray;
+import java.time.Instant;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Converter for {@link io.vertx.armysystem.business.common.standard.PBFStandard}.
+ * NOTE: This class has been automatically generated from the {@link io.vertx.armysystem.business.common.standard.PBFStandard} original class using Vert.x codegen.
+ */
+public class PBFStandardConverter {
+
+  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, PBFStandard obj) {
+    for (java.util.Map.Entry<String, Object> member : json) {
+      switch (member.getKey()) {
+        case "ageFrom":
+          if (member.getValue() instanceof Number) {
+            obj.setAgeFrom(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "ageTo":
+          if (member.getValue() instanceof Number) {
+            obj.setAgeTo(((Number)member.getValue()).intValue());
+          }
+          break;
+        case "gender":
+          if (member.getValue() instanceof String) {
+            obj.setGender((String)member.getValue());
+          }
+          break;
+        case "id":
+          if (member.getValue() instanceof String) {
+            obj.setId((String)member.getValue());
+          }
+          break;
+        case "pbfFrom":
+          if (member.getValue() instanceof Number) {
+            obj.setPbfFrom(((Number)member.getValue()).doubleValue());
+          }
+          break;
+        case "pbfTo":
+          if (member.getValue() instanceof Number) {
+            obj.setPbfTo(((Number)member.getValue()).doubleValue());
+          }
+          break;
+        case "standardId":
+          if (member.getValue() instanceof String) {
+            obj.setStandardId((String)member.getValue());
+          }
+          break;
+      }
+    }
+  }
+
+  public static void toJson(PBFStandard obj, JsonObject json) {
+    toJson(obj, json.getMap());
+  }
+
+  public static void toJson(PBFStandard obj, java.util.Map<String, Object> json) {
+    json.put("ageFrom", obj.getAgeFrom());
+    json.put("ageTo", obj.getAgeTo());
+    if (obj.getGender() != null) {
+      json.put("gender", obj.getGender());
+    }
+    if (obj.getId() != null) {
+      json.put("id", obj.getId());
+    }
+    if (obj.getPbfFrom() != null) {
+      json.put("pbfFrom", obj.getPbfFrom());
+    }
+    if (obj.getPbfTo() != null) {
+      json.put("pbfTo", obj.getPbfTo());
+    }
+    if (obj.getStandardId() != null) {
+      json.put("standardId", obj.getStandardId());
+    }
+  }
+}
---
+++ backend/business-common/src/main/java/io/vertx/armysystem/business/common/standard/PBFStandard.java
@@ -0,0 +1,95 @@
+package io.vertx.armysystem.business.common.standard;
+
+import io.vertx.codegen.annotations.DataObject;
+import io.vertx.core.json.JsonObject;
+
+/**
+ * 体脂合格标准
+ *
+ * @author Derek Zheng
+ */
+@DataObject(generateConverter = true)
+public class PBFStandard {
+  private String id;
+  private String standardId;          // 训练大纲Id
+  private String gender;              // 性别
+  private int ageFrom;                // 年龄范围
+  private int ageTo;
+  private Double pbfFrom;             // 体脂范围,单位百分比
+  private Double pbfTo;
+
+  public PBFStandard() {
+    // Empty constructor
+  }
+
+  public PBFStandard(JsonObject json) {
+    PBFStandardConverter.fromJson(json, this);
+  }
+
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    PBFStandardConverter.toJson(this, json);
+    return json;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getStandardId() {
+    return standardId;
+  }
+
+  public void setStandardId(String standardId) {
+    this.standardId = standardId;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public int getAgeFrom() {
+    return ageFrom;
+  }
+
+  public void setAgeFrom(int ageFrom) {
+    this.ageFrom = ageFrom;
+  }
+
+  public int getAgeTo() {
+    return ageTo;
+  }
+
+  public void setAgeTo(int ageTo) {
+    this.ageTo = ageTo;
+  }
+
+  public Double getPbfFrom() {
+    return pbfFrom;
+  }
+
+  public void setPbfFrom(Double pbfFrom) {
+    this.pbfFrom = pbfFrom;
+  }
+
+  public Double getPbfTo() {
+    return pbfTo;
+  }
+
+  public void setPbfTo(Double pbfTo) {
+    this.pbfTo = pbfTo;
+  }
+
+  @Override
+  public String toString() {
+    return toJson().encodePrettily();
+  }
+}
